360e8b1f26fe091b155a4143aca47cbdee9939fb
GuteeQ <wiktor.gut08@icloud.com>
2024-02-28 22:02:50 +0100

    New File - MailcapParseException.java (4/6780)

---
+++ src/V1/com/GuTek/activation/registeries/MailcapParseException.java
@@ -0,0 +1,21 @@
+/*
+    Author: .GuTeK <dev@gutcode.pl>
+    Project: GUT-ENGINE [MINECRAFT ENGINE]
+    Price: WEBSITE
+    Resources: 4/6780
+    Data: 28.02.2024
+    Contact Discord: .GuTeK#0001
+    Contact e-mail: dev@gutcode.pl
+    Our websites: https://gutcode.pl
+    ⓒ 2024 by .GuTeK | ALL RIGHTS RESERVED |
+*/
+
+package V1.com.GuTek.activation.registeries;
+
+public class MailcapParseException extends Exception {
+    public MailcapParseException() {
+        public MailcapParseException(final String inInfo) {
+            super(inInfo);
+        }
+    }
+}
